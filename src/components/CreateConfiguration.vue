6e44d711c1af33f0e173bc2db4308db3c6bed289
Alphius <alphiuswambua@gmail.com>
2024-08-20 23:33:59 +0300

    Added Tailwind css and setup pages

---
+++ src/components/CreateConfiguration.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Create Config</h1>
+</template>
---
+++ src/views/CreateConfiguration.vue
@@ -0,0 +1,9 @@
+<script>
+import ConfigurationListVue from '../components/ConfigurationList.vue'
+</script>
+
+<template>
+  <div class="about">
+    <h1>Create Configuration</h1>
+  </div>
+</template>
