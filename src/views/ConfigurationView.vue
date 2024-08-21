c056c590d1f93779f59fd7b164924d12dd37534b
Alphius <alphiuswambua@gmail.com>
2024-08-21 13:12:26 +0300

    Ui clean up

--- src/views/ConfigurationView.vue
+++ src/views/ConfigurationView.vue
@@ -41,7 +41,6 @@ const handleDelete = async () => {
   }
 }
 </script>
-
 <template>
   <BackButton />
   <section v-if="!loading && configuration" class="bg-green-50">
@@ -49,8 +48,7 @@ const handleDelete = async () => {
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
         <main>
           <div class="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-            <div class="text-gray-500 mb-4">{{ configuration.content?.type }}</div>
-            <h1 class="text-3xl font-bold mb-4">{{ configuration.title }}</h1>
+            <h1 class="text-3xl font-bold mb-4">{{ configuration.name }}</h1>
             <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
               <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
               <p class="text-orange-700">{{ configuration.location }}</p>
@@ -60,24 +58,30 @@ const handleDelete = async () => {
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-green-800 text-lg font-bold mb-6">Configuration Description</h3>
             <p class="mb-4">{{ configuration.description }}</p>
-            <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
-            <p class="mb-4">{{ configuration.salary }} / Year</p>
+          </div>
+
+          <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+            <h3 class="text-green-800 text-lg font-bold mb-6">Content Details</h3>
+            <div v-if="configuration.content">
+              <div v-for="(value, key) in configuration.content" :key="key" class="mb-2">
+                <strong class="text-gray-600">{{ key }}:</strong>
+                <span v-if="Array.isArray(value)">
+                  <!-- Display array values -->
+                  <ul class="list-disc list-inside">
+                    <li v-for="item in value" :key="item">{{ item }}</li>
+                  </ul>
+                </span>
+                <span v-else>
+                  <!-- Display other types of values -->
+                  {{ value }}
+                </span>
+              </div>
+            </div>
           </div>
         </main>
 
         <!-- Sidebar -->
         <aside>
-          <div class="bg-white p-6 rounded-lg shadow-md">
-            <h3 class="text-xl font-bold mb-6">Company Info</h3>
-            <h2 class="text-2xl">{{ configuration?.name }}</h2>
-            <p class="my-2">{{ configuration?.description }}</p>
-            <hr class="my-4" />
-            <h3 class="text-xl">Contact Email:</h3>
-            <p class="my-2 bg-green-100 p-2 font-bold">{{ configuration?.contactEmail }}</p>
-            <h3 class="text-xl">Contact Phone:</h3>
-            <p class="my-2 bg-green-100 p-2 font-bold">{{ configuration?.contactPhone }}</p>
-          </div>
-
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Configuration</h3>
             <RouterLink
